0e8f5eb0a52847e91539a93505ea4d4976c6ab60
Daniel <d3637042@gmail.com>
2018-10-18 02:44:59 +0800

    add docker

---
+++ docker/gym-gazebo-wamv/build.bash
@@ -0,0 +1,1 @@
+docker build --tag ros-kinetic-gazebo-gym-nvidia9.0 .
---
+++ docker/gym-gazebo-wamv/run.bash
@@ -0,0 +1,13 @@
+docker run \
+  --runtime=nvidia \
+    -v="/tmp/.gazebo/:/root/.gazebo/" \
+    --rm \
+    -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -p 11345:11345 \
+    -p 11311:11311 \
+    --name=ros-kinetic-gazebo-gym-nvidia9.0 \
+    ros-kinetic-gazebo-gym-nvidia9.0:latest \
+    /bin/bash
