e54960ea710559a5f77cb036a23510d303fa0ff1
Daniel <d3637042@gmail.com>
2018-10-22 16:32:26 +0800

    add docker for non-gpu machines

---
+++ docker/gym-gazebo-wamv/kinetic/build.bash
@@ -0,0 +1,1 @@
+docker build --tag d3637042/ros-kinetic-gazebo-gym .
---
+++ docker/gym-gazebo-wamv/kinetic/run.bash
@@ -0,0 +1,13 @@
+docker run \
+  --runtime=nvidia \
+    -v="/tmp/.gazebo/:/root/.gazebo/" \
+    --rm \
+    -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -p 11345:11345 \
+    -p 11311:11311 \
+    --name=ros-kinetic-gazebo-gym \
+    d3637042/ros-kinetic-gazebo-gym:latest \
+    /bin/bash
